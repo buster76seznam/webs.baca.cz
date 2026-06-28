a4680d5b631e3a491fec93375fd3d807b81e3ce0
finoritradeprivacy <finoritrade.privacy@gmail.com>
2026-06-28 20:42:35 +0200

    Fix table name in SQL migration (orders instead of web_orders)

--- supabase/add_contract_fields.sql
+++ supabase/add_contract_fields.sql
@@ -1,6 +1,6 @@
--- Add contract fields to web_orders table
+-- Add contract fields to orders table
 
-ALTER TABLE web_orders
+ALTER TABLE orders
   ADD COLUMN IF NOT EXISTS legal_business_name TEXT,
   ADD COLUMN IF NOT EXISTS state_of_incorporation TEXT,
   ADD COLUMN IF NOT EXISTS principal_place_of_business TEXT,
@@ -8,8 +8,8 @@ ALTER TABLE web_orders
   ADD COLUMN IF NOT EXISTS contract_email TEXT;
 
 -- Add comments
-COMMENT ON COLUMN web_orders.legal_business_name IS 'Přesný právní název firmy včetně koncovky (např. LLC, Inc.)';
-COMMENT ON COLUMN web_orders.state_of_incorporation IS 'Stát, kde je firma registrovaná';
-COMMENT ON COLUMN web_orders.principal_place_of_business IS 'Kompletní oficiální adresa sídla firmy';
-COMMENT ON COLUMN web_orders.authorized_signatory IS 'Jméno osoby s jednatelským oprávněním (majitel/CEO)';
-COMMENT ON COLUMN web_orders.contract_email IS 'E-mail pro výzvy k podpisu a platbám';
+COMMENT ON COLUMN orders.legal_business_name IS 'Přesný právní název firmy včetně koncovky (např. LLC, Inc.)';
+COMMENT ON COLUMN orders.state_of_incorporation IS 'Stát, kde je firma registrovaná';
+COMMENT ON COLUMN orders.principal_place_of_business IS 'Kompletní oficiální adresa sídla firmy';
+COMMENT ON COLUMN orders.authorized_signatory IS 'Jméno osoby s jednatelským oprávněním (majitel/CEO)';
+COMMENT ON COLUMN orders.contract_email IS 'E-mail pro výzvy k podpisu a platbám';
